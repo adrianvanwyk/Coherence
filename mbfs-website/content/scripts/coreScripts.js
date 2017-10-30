5647d3eb5d5c21c324fb08b0d110ab781554355b
Michal Malujlo <michalmal@hotmail.co.za>
2017-10-30 14:38:39 +0200

    header banner

--- mbfs-website/content/scripts/coreScripts.js
+++ mbfs-website/content/scripts/coreScripts.js
@@ -1,18 +1,17 @@
-$(document).ready(function(){
-$("#slider > .div:gt(0)").hide();
-slideShow();
+$(document).ready(function() {
+  function bslideShow() {
+    $("#slider > div:gt(0)").hide();
+  };
 
-function slideShow(){
+  function slideShow() {
+    var current = $('#slider > div:not(:hidden)');
+    current.css('z-index', '1');
+    var next = current.next().length ? current.next() : current.siblings().first();
+    next.css('z-index', '0').show();
+    current.fadeOut();
 
-  var current = $('#slider > div:not(:hidden)');
-  alert(current.attr('class'));
-var next = current.next().length ? current.next() : current.siblings().first();
-
- current.fadeOut();
- next.fadeIn();
-
-setTimeout(slideShow, 3000);
-
-};
+  };
+  bslideShow();
+  setInterval(slideShow, 5000);
 
 });
